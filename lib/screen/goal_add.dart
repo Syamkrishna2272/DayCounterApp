cad88dcc84299eb2157fb28788b1abb65418773e
Syamkrishna2272 <syamkrishnashyam@gmail.com>
2024-08-10 15:58:02 +0530

    goal adding completed

--- lib/screen/goal_add.dart
+++ lib/screen/goal_add.dart
@@ -1,11 +1,159 @@
+import 'package:flutter/material.dart';
 
-import 'package:flutter/widgets.dart';
-
-class GoalAddingScreen extends StatelessWidget {
-  const GoalAddingScreen({super.key});
+class GoalAdding extends StatelessWidget {
+  const GoalAdding({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius:
+            BorderRadius.circular(MediaQuery.sizeOf(context).width / 25),
+      ),
+      backgroundColor: const Color(0xFF34343E),
+      child: Container(
+        padding: EdgeInsets.all(MediaQuery.sizeOf(context).width / 20),
+        width: MediaQuery.sizeOf(context).width * 0.8,
+        height: MediaQuery.sizeOf(context).height * 0.6,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Text(
+                    "Create your Goal",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                      fontSize: MediaQuery.sizeOf(context).width / 20,
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(
+                    Icons.close,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
+            const Text(
+              "Create Your Countdown Timer. Start tracking your",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            const Text(
+              "journey to success today!",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: MediaQuery.sizeOf(context).height / 25),
+            const Text(
+              "Title*",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+            ),
+            SizedBox(
+              height: MediaQuery.sizeOf(context).height / 15,
+              child: TextField(
+                textCapitalization: TextCapitalization.words,
+                decoration: InputDecoration(
+                  hintText: "Enter title (Mandatory)",
+                  hintStyle: const TextStyle(color: Color(0xFFB0B0B0)),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                        MediaQuery.sizeOf(context).width / 30),
+                  ),
+                  fillColor: const Color(0xFF121212),
+                  filled: true,
+                ),
+                style: TextStyle(
+                    fontSize: MediaQuery.sizeOf(context).height / 50,
+                    color: Colors.white),
+                cursorColor: Colors.white,
+              ),
+            ),
+            SizedBox(height: MediaQuery.sizeOf(context).width / 30),
+            const Text(
+              "Number of Days*",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  height: MediaQuery.sizeOf(context).height / 15,
+                  width: MediaQuery.sizeOf(context).width / 3,
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      hintText: "Days (Mandatory)",
+                      hintStyle: const TextStyle(color: Color(0xFFB0B0B0)),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(
+                            MediaQuery.sizeOf(context).width / 30),
+                      ),
+                      fillColor: const Color(0xFF121212),
+                      filled: true,
+                    ),
+                    style: TextStyle(
+                        fontSize: MediaQuery.sizeOf(context).height / 60,
+                        color: Colors.white),
+                    cursorColor: Colors.white,
+                  ),
+                ),
+                Container(
+                  height: MediaQuery.sizeOf(context).height / 16,
+                  width: MediaQuery.sizeOf(context).width / 3,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF121212),
+                    borderRadius: BorderRadius.circular(
+                        MediaQuery.sizeOf(context).width / 30),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      "DAYS",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(
+                    vertical: MediaQuery.sizeOf(context).height / 50),
+                backgroundColor: const Color(0xFF2C69DE),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(
+                      MediaQuery.sizeOf(context).width / 30),
+                ),
+              ),
+              child: const Text(
+                "Start Countdown",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/screen/goal_screen.dart
+++ lib/screen/goal_screen.dart
@@ -1,3 +1,4 @@
+import 'package:day_counter/screen/goal_add.dart';
 import 'package:flutter/material.dart';
 
 class GoalScreen extends StatelessWidget {
@@ -6,6 +7,7 @@ class GoalScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
         backgroundColor: const Color(0xFF121212),
         appBar: AppBar(
           backgroundColor: const Color(0xFF121212),
@@ -49,103 +51,11 @@ class GoalScreen extends StatelessWidget {
                 backgroundColor: const Color(0xFF2C69DE),
                 onPressed: () {
                   showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return Dialog(
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(
-                                  MediaQuery.sizeOf(context).width / 25),
-                              color: const Color(0xFF34343E),
-                            ),
-                            height: MediaQuery.sizeOf(context).height / 2.2,
-                            width: MediaQuery.sizeOf(context).width * 0.8,
-                            child: Column(
-                              children: [
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Expanded(
-                                      child: Text(
-                                        "Create your Goal",
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.w600,
-                                          fontSize:
-                                              MediaQuery.sizeOf(context).width /
-                                                  20,
-                                        ),
-                                      ),
-                                    ),
-                                    IconButton(
-                                      icon: const Icon(
-                                        Icons.close,
-                                        color: Colors.white,
-                                      ),
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                    ),
-                                  ],
-                                ),
-                                const Text(
-                                  "Create Your Countdown Timer. Start tracking your",
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                const Text(
-                                  "journey to success today!",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                SizedBox(
-                                    height:
-                                        MediaQuery.sizeOf(context).height / 25),
-                                const Text(
-                                  "Title*",
-                                  textAlign: TextAlign.left,
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                Container(
-                                  height:
-                                      MediaQuery.sizeOf(context).height / 15,
-                                  width: MediaQuery.sizeOf(context).width / 1.4,
-                                  color: const Color(0xFF121212),
-                                  child: TextField(
-                                    decoration: InputDecoration(
-                                      hintText: "Enter title (Mandatory)",
-                                      hintStyle: const TextStyle(
-                                        color: Color(0xFF34343E),
-                                      ),
-                                      border: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(
-                                            MediaQuery.sizeOf(context).width /
-                                                30),
-                                      ),
-                                      contentPadding:
-                                          const EdgeInsets.symmetric(
-                                              horizontal: 16.0),
-                                    ),
-                                    style: TextStyle(
-                                      fontSize:
-                                          MediaQuery.sizeOf(context).height /
-                                              50,
-                                    ),
-                                    cursorColor: Colors.black,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        );
-                      });
+                    context: context,
+                    builder: (BuildContext context) {
+                      return const GoalAdding();
+                    },
+                  );
                 },
                 shape: const CircleBorder(),
                 child: const Icon(
